d7babdffadcc030cae1c5b81e9b4f21ea21174ba
shemul <shemulworld@gmail.com>
2015-07-10 05:50:58 +0600

    added post Method and loginData

--- src/com/sgk/Action.java
+++ src/com/sgk/Action.java
@@ -1,6 +1,10 @@
 package com.sgk ;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -15,14 +19,16 @@ import org.json.JSONObject;
  * @author shemul
  */
 public class Action {
-    private static String url = "http://128.199.201.52/app/SirGeloKoi/info";
+    private static String urlForInformation = "http://128.199.201.52/app/SirGeloKoi/info";
+    private static String urlForLogin = "http://128.199.201.52/app/SirGeloKoi/login";
     JSONArray  status = null;
+    JSONArray  login = null;
+           
     ServiceHandler sh = new ServiceHandler();
-    String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
-        
-    public Info[] runit() {
-        
-        //System.out.println("Response " + jsonStr);
+    String jsonStr = sh.makeServiceCall(urlForInformation, ServiceHandler.GET);
+    String jsonLogin = sh.makeServiceCall(urlForLogin, ServiceHandler.GET);
+           
+    public Info[] getInformation() {
         
         Info in[] = new Info[getStatusCount()];
          for (int i = 0; i < in.length; i++) {
@@ -55,7 +61,49 @@ public class Action {
         }
         return in;
     }
-     
+    
+     public int getLoginData(String u , String p) {
+       
+         
+        boolean loginStatus = false ; 
+        int userStatus = 0 ;
+        if (jsonLogin != null) {
+            try {
+                    JSONObject jsonObj = new JSONObject(jsonLogin);
+                   
+                    login = jsonObj.getJSONArray("login");
+                    
+                    for (int i = 0; i < login.length(); i++) {
+                        
+                        JSONObject c = login.getJSONObject(i);
+                       // System.out.println("Found");
+                       // System.out.println(c.getString("username"));
+                       if (u.equals(c.getString("username")) && p.equals(c.getString("password"))) {
+                           if("1".equals(c.getString("status"))) {
+                               if("1".equals(c.getString("isAdmin"))) {
+                                   userStatus = 1 ;
+                                   break;
+                               } else {
+                                    userStatus = 2 ;
+                                    break;
+                               }
+                           } else {
+                               userStatus = 3 ;
+                           }
+                           
+                       } else {
+                           userStatus = 0;
+                       }
+                    }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+       return userStatus;
+        //return 1;
+    }
+    
     public int getStatusCount() {
         if (jsonStr != null) {
             try {
@@ -68,5 +116,31 @@ public class Action {
             }
         }
         return status.length();
+    }
+    
+    public void postMet(){
+        ServiceHandler sh = new ServiceHandler();
+        String url3 = "http://128.199.201.52/app/SirGeloKoi/login";
+			// Making a request to url and getting response
+        List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(2);
+         nameValuePair.add(new BasicNameValuePair("search", "shemul"));
+         
+	String jsonStr4 = sh.makeServiceCall(url3, ServiceHandler.POST, nameValuePair);
+         
+         
+         if (jsonStr4 != null) {
+            try {
+                    JSONObject jsonObj = new JSONObject(jsonStr4);
+                   
+                   // status = jsonObj.getJSONArray("login");
+                    
+                    System.out.println(jsonObj.toString());
+                    
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        //return status.length();
+        
     }
 }
